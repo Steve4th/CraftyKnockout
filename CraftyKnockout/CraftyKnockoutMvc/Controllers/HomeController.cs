52aa5fb07586d7485b076c8231d8a9dd019e7ac2
Steve 4th <steveforth@hotmail.com>
2013-11-08 22:29:21 +0000

    Home page updates

--- CraftyKnockout/CraftyKnockoutMvc/Controllers/HomeController.cs
+++ CraftyKnockout/CraftyKnockoutMvc/Controllers/HomeController.cs
@@ -1,23 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CraftyKnockoutMvc.Controllers
 {
     public class HomeController : Controller
     {
+        const string ApplicationAbstract = "A look into crafting web application with KnockoutJS and ASP.NET MVC. Discussing what knockout brings to the toolbox and when to use it.";
+
         public ActionResult Index()
         {
+            ViewBag.Title = "Home Page";
+            ViewBag.Abstract = ApplicationAbstract;
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Title = "About Crafty Knockout";
-            ViewBag.Message = "Crafty Knockout is an ASP.NET application created to facilite a presentation about KnockuutJS";
-            ViewBag.Abstract = "A look into crafting web apps with KnockoutJS and ASP.NET MVC. Discussing what knockout brings to the toolbax and when to use it.";
+            ViewBag.Message = "Crafty Knockout is an ASP.NET application created to facilitate a presentation about KnockuutJS";
+            ViewBag.Abstract = ApplicationAbstract;
 
             return View();
         }
@@ -25,7 +25,7 @@ namespace CraftyKnockoutMvc.Controllers
         public ActionResult Contact()
         {
             ViewBag.Title = "Contact";
-            ViewBag.Message = "Contact page.";
+            ViewBag.Message = "This presentation application has been created by Steve Forth";
 
             return View();
         }
