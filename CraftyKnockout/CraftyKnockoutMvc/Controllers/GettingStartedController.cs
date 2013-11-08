7fd559ba91cb3f41b9740c499d00f3425b7f30cc
Steve 4th <steveforth@hotmail.com>
2013-11-08 23:05:16 +0000

    Added Nuget Packages and created Getting started controller

---
+++ CraftyKnockout/CraftyKnockoutMvc/Controllers/GettingStartedController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Web.Mvc;
+
+namespace CraftyKnockoutMvc.Controllers
+{
+    public class GettingStartedController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Getting started with KnockoutJS";
+            ViewBag.Message = "An introduction to the KnockoutJS basics. A reminder for those familiar and an introduction for the uninitiated.";
+            return View();
+        }
+
+        public ActionResult Resources()
+        {
+            return View();
+        }
+	}
+}
