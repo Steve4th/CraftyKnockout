c73902c6a798789f463bbb6253ce262e5e13d2db
Steve 4th <steveforth@hotmail.com>
2013-11-13 23:02:17 +0000

    updated computed layout and calculation

--- CraftyKnockout/CraftyKnockoutMvc/Scripts/GettingStarted/Computed.js
+++ CraftyKnockout/CraftyKnockoutMvc/Scripts/GettingStarted/Computed.js
@@ -5,11 +5,11 @@
     self.VatRate = ko.observable(20.00);
 
     self.VatAmount = ko.computed(function () {
-        return self.NetPrice() * (self.VatRate() / 100).toFixed(2);
+        return (self.NetPrice() * (self.VatRate() / 100)).toFixed(2);
     });
 
     self.GrossPrice = ko.computed(function () {
-        return self.NetPrice() + (self.NetPrice() * (self.VatRate() / 100)).toFixed(2);
+        return  ((self.NetPrice() * (self.VatRate() / 100)) + parseFloat(self.NetPrice())).toFixed(2);
     });
 }
 
