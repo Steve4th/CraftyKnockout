0cde665f5e8440e86fc0cb6eaa4a0971eb357a02
Steve 4th <steveforth@hotmail.com>
2013-11-15 20:26:31 +0000

    added from jsonattrribute

---
+++ CraftyKnockout/CraftyKnockoutMvc/Attributes/FromJsonAttribute.cs
@@ -0,0 +1,39 @@
+﻿namespace CraftyKnockoutMvc.Attributes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Script.Serialization;
+
+    public sealed class FromJsonAttribute : CustomModelBinderAttribute
+    {
+        private readonly static JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+        public override IModelBinder GetBinder()
+        {
+            return new JsonModelBinder();
+        }
+
+        private class JsonModelBinder : IModelBinder
+        {
+            public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+            {
+                if (bindingContext == null || controllerContext == null)
+                {
+                    return null;
+                }
+
+                var stringified = controllerContext.HttpContext.Request[bindingContext.ModelName];
+
+                if (string.IsNullOrEmpty(stringified))
+                {
+                    return null;
+                }
+
+                return serializer.Deserialize(stringified, bindingContext.ModelType);
+            }
+        }
+    }
+}
--- CraftyKnockout/CraftyKnockoutMvc/Scripts/GettingStarted/ObservableArray.js
+++ CraftyKnockout/CraftyKnockoutMvc/Scripts/GettingStarted/ObservableArray.js
@@ -1,9 +1,7 @@
 ﻿function CoderProfile() {
-    coderName = ko.observable('EnterName');
-    score = ko.observable(0);
-    famousFor = ko.observable('Enter your claim to fame');
-    presentationDate = ko.observable(Date.now());
-    currentRecord = ko.observable(false);
+    coderName = ko.observable('EnterName'),
+    score = ko.observable(0),
+    famousFor = ko.observable('Enter a claim to fame')
 };
 
 function HallOfFrameViewModel() {
@@ -11,26 +9,39 @@ function HallOfFrameViewModel() {
 
     self.FamousCoders = ko.observableArray([]);
 
-    self.currentRecord = ko.observable();
+    self.EditableRecord = ko.observable();
 
     self.AddCoder = function () {
         var coder = new CoderProfile();
         self.FamousCoders.push(coder);
-        self.currentRecord(coder);
+        self.EditableRecord(coder);
     };
 
-    self.RemoveCoder = function(record) {
+    self.RemoveCoder = function (record) {
+        if (record == self.EditableRecord()) {
+            self.EditableRecord(null);
+        }
         self.FamousCoders.remove(record);
     };
 
-    self.EditCoder = function (record) {
-        record.currentRecord(true);
-        self.currentRecord(record);
+    self.EditCoder = function (data) {
+        self.EditableRecord(data);
     };
 
     self.SortCoders = function () {
-        self.FamousCoders().sort(function (left, right) {
-            return left.score == right.score ? 0 : (left.score < right.score ? -1 : 1)
+        self.FamousCoders.sort(function (left, right) {
+            console.log(ko.utils.unwrapObservable(left));
+            if (left.score() == right.score()) {
+                return 0;
+            }
+            else {
+                if (left.score() < right.score()) {
+                    return -1;
+                }
+                else {
+                    return 1;
+                }
+            }
         });
     };
 }
