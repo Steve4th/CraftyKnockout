cee6618c813f4daa474e7043b42de70063a4979c
Steve 4th <steveforth@hotmail.com>
2013-11-17 22:51:42 +0000

    re-factored the HallOfFame controller action

--- CraftyKnockout/CraftyKnockoutMvc/Controllers/IntegrationWithMvcController.cs
+++ CraftyKnockout/CraftyKnockoutMvc/Controllers/IntegrationWithMvcController.cs
@@ -1,6 +1,7 @@
 ﻿using CraftyKnockoutMvc.Models;
 using System.Collections.Generic;
 using System.Web.Mvc;
+using System.Linq;
 
 namespace CraftyKnockoutMvc.Controllers
 {
@@ -17,32 +18,41 @@ namespace CraftyKnockoutMvc.Controllers
         {
             var model = new HallOfFameModel();
 
-            var coder = new FamousCoder()
-            {
-                CoderName = "Jon Skeet",
-                FameScore = 20,
-                FamousFor = "Stack overflow"
-            };
-            
-            var coder2 = new FamousCoder()
-            {
-                CoderName = "Douglas Crockford",
-                FameScore = 10,
-                FamousFor = "JavaScript"
-            };
+            var listOfFamousCoders = GetFamousCoders();
 
-            var coder3 = new FamousCoder()
+            foreach (var coder in listOfFamousCoders)
             {
-                CoderName = "Scott Guthrie",
-                FameScore = 25,
-                FamousFor = "Red Shirt"
-            };
-
-            model.FamousCoders.Add(coder);
-            model.FamousCoders.Add(coder2);
-            model.FamousCoders.Add(coder3);
+                model.FamousCoders.Add(coder);
+            }
 
             return View(model);
+        }
+
+        private static IList<FamousCoder> GetFamousCoders()
+        {
+            var returnList = new List<FamousCoder>()
+            {
+                new FamousCoder()
+                {
+                    CoderName = "Jon Skeet",
+                    FameScore = 20,
+                    FamousFor = "Stack overflow"
+                },
+                new FamousCoder()
+                {
+                    CoderName = "Douglas Crockford",
+                    FameScore = 10,
+                    FamousFor = "JavaScript"
+                },
+                new FamousCoder()
+                   {
+                       CoderName = "Scott Guthrie",
+                       FameScore = 25,
+                       FamousFor = "Red Shirt"
+                   }
+            };
+
+            return returnList;
         }
 	}
 }
