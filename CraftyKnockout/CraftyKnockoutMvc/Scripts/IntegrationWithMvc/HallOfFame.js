b8858ccb7699b6a7e54885279e79b801731b7e67
Steve 4th <steveforth@hotmail.com>
2013-11-17 22:21:34 +0000

    Simplified the hall of fame example

--- CraftyKnockout/CraftyKnockoutMvc/Scripts/IntegrationWithMvc/HallOfFame.js
+++ CraftyKnockout/CraftyKnockoutMvc/Scripts/IntegrationWithMvc/HallOfFame.js
@@ -1,16 +1,8 @@
-﻿function HallOfFameViewModel(coderList) {
-    self = this;
-
-    // Use the Knockout Mapping utility function to convert the supplied JSON into an observable array
-    self.FamousCoders = ko.mapping.fromJSON(coderList);
-}
-
-
-$(document).ready(function () {
+﻿$(document).ready(function () {
     //get our list of codes in JSON form to use to seed the knockout observable array
     var listOfCoders = $('#initialCoderListJson').val();
 
-    var viewModel = new HallOfFameViewModel(listOfCoders);
+    var viewModel = ko.mapping.fromJSON(listOfCoders);
 
     ko.applyBindings(viewModel);
 });
