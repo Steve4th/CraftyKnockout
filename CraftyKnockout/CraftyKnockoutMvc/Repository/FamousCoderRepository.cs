0940a47a3dc3d0bcfc586d651ca996e967f601bb
Steve 4th <steveforth@hotmail.com>
2013-11-18 08:59:13 +0000

    Added repository for storing famous coders

---
+++ CraftyKnockout/CraftyKnockoutMvc/Repository/FamousCoderRepository.cs
@@ -0,0 +1,39 @@
+﻿namespace CraftyKnockoutMvc.Repository
+{
+    using CraftyKnockoutMvc.Models;
+    using System;
+    using System.Linq;
+
+    public class FamousCoderRepository: IRepository<FamousCoder>
+    {
+        public IQueryable<FamousCoder> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<FamousCoder> Get(params System.Linq.Expressions.Expression<Func<FamousCoder, object>>[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FamousCoder Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertOrUpdate(FamousCoder address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CraftyKnockout/CraftyKnockoutMvc/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CraftyKnockoutMvc.Repository
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> Get(params Expression<Func<T, object>>[] includeProperties);
+        T Get(int id);
+        void InsertOrUpdate(T address);
+        void Delete(int id);
+        void Save();
+    }
+}
