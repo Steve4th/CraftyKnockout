29cdc299b2172e193a94e9dca0da533782b9dd63
Steve 4th <steveforth@hotmail.com>
2013-11-18 00:00:55 +0000

    template and viewmodel for editable hall of fame

--- CraftyKnockout/CraftyKnockoutMvc/Scripts/IntegrationWithMvc/EditHallOfFame.js
+++ CraftyKnockout/CraftyKnockoutMvc/Scripts/IntegrationWithMvc/EditHallOfFame.js
@@ -1,8 +1,32 @@
-﻿$(document).ready(function () {
-    //get our list of codes in JSON form to use to seed the knockout observable array
+﻿function EditableHallOfFrame(initialListOfCoders) {
+    self = this;
+
+    self.FamousCoders = ko.mapping.fromJSON(initialListOfCoders);
+
+    self.FamousCoderCount = ko.computed(function () {
+        //return self.FamousCoders.length;
+    });
+
+    self.RemoveCoder = function (record) {
+        //self.FamousCoders.remove(record);
+    };
+
+    self.SaveIt = function () {
+        //postback the model as JSON to the server
+        ko.utils.postJson(location.href, { model: self });
+
+        //// Use the ko.toJS to convert the array into POJO's. 
+        //// This way when encoded by the postJson method the observable properties are also included,
+        ////var jsDataPayload = ko.toJS(self.NumberdwellingTypes);
+        ////ko.utils.postJson(location.href, { NumberdwellingTypes: jsDataPayload });
+    };
+}
+
+$(document).ready(function () {
+    //get our list of coders in JSON form to use to seed the knockout observable array
     var listOfCoders = $('#initialCoderListJson').val();
 
-    var viewModel = ko.mapping.fromJSON(listOfCoders);
+    var viewModel = EditableHallOfFrame(listOfCoders);
 
     ko.applyBindings(viewModel);
 });
