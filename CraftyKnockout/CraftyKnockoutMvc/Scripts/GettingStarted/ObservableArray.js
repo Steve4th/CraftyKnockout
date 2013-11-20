9d6af965dcbb931c4d1394658d81562ae9bb0af8
Steve 4th <steveforth@hotmail.com>
2013-11-20 23:42:52 +0000

    sorting, active selection and style of observable array

--- CraftyKnockout/CraftyKnockoutMvc/Scripts/GettingStarted/ObservableArray.js
+++ CraftyKnockout/CraftyKnockoutMvc/Scripts/GettingStarted/ObservableArray.js
@@ -1,56 +1,53 @@
 ﻿var CoderProfile = function () {
-    this.coderName = ko.observable('EnterName');
+    this.coderName = ko.observable();
     this.fameScore = ko.observable(0);
-    this.famousFor = ko.observable('Enter a claim to fame');
+    this.famousFor = ko.observable();
+    this.isSelected = ko.observable(false);
 };
 
-
-function CompareCoder(left, right) {
-    console.log(ko.utils.unwrapObservable(left));
-    if (left.fameScore() === right.fameScore()) {
-        return 0;
-    }
-    else {
-        if (left.fameScore() < right.fameScore()) {
-            return -1;
-        }
-        else {
-            return 1;
-        }
-    }
-}
 
 function HallOfFrameViewModel() {
     self = this;
 
     self.FamousCoders = ko.observableArray([]);
 
-    self.EditableRecord = ko.observable();
-
     self.AddCoder = function () {
+        //create a new coder using our declared function. You must set some default values for you new object here too.
         var coder = new CoderProfile()
             .fameScore(0)
             .coderName('')
             .famousFor('');
+
         self.FamousCoders.push(coder);
-        self.EditableRecord(coder);
+        self.EditCoder(coder);
+        console.log('Added Coder');
     };
 
     self.RemoveCoder = function (record) {
+        console.log('Removing Coder');
         if (record == self.EditableRecord()) {
             self.EditableRecord(null);
         }
         self.FamousCoders.remove(record);
     };
 
+    self.EditableRecord = ko.observable();
+
     self.EditCoder = function (data) {
+        if (self.EditableRecord()) {
+            self.EditableRecord().isSelected(false);
+        }
+        data.isSelected(true);
         self.EditableRecord(data);
     };
 
     self.SortCoders = function () {
-        self.FamousCoders.sort(CompareCoder);
+        console.log('Sorting Coders');
+        self.FamousCoders.sort(
+            function CompareScore(left, right) {
+                return right.fameScore() - left.fameScore();
+            });
     };
-
 }
 
 
