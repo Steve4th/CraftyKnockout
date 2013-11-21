52eb12943e8ac1d41ff1af292b334c1d91e0f2a9
Steve 4th <steveforth@hotmail.com>
2013-11-21 11:19:51 +0000

    Fixed event date format

--- CraftyKnockout/CraftyKnockoutMvc/Models/Event.cs
+++ CraftyKnockout/CraftyKnockoutMvc/Models/Event.cs
@@ -18,7 +18,7 @@ namespace CraftyKnockoutMvc.Models
         public string Location { get; set; }
 
         [Display(Name = "Date of Event")]
-        [DisplayFormat(DataFormatString="{0:dd MMM yyyy}", ApplyFormatInEditMode=true)]
+        [DisplayFormat(DataFormatString="{0:D}", ApplyFormatInEditMode=true)]
         public DateTime? DateOfEvent { get; set; }
 
         public IList<FamousCoder> Speakers { get; set; }
